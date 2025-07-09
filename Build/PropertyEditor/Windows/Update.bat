692c7f00ee66270c802509e7ceeed0cf071e9753
sainimu78 <393584307@qq.com>
2025-07-10 01:17:13 +0800

    windows-2019 退役, 修复 v140 以上 Tool set 时 Qt 5.8 编译错误

--- Build/PropertyEditor/Windows/Update.bat
+++ Build/PropertyEditor/Windows/Update.bat
@@ -1,4 +1,4 @@
 @echo off
 @set ProjectDirPath=..\..\..\..\Project\PropertyEditor
-@set Toolset=v140
+::@set Toolset=v140
 @..\..\..\ThirdParty\CMakeProjectFramework\PlatformSpecificScript\Windows\Update.bat
